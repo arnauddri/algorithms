f5ff6e0032ab214500257a165ddab8e19ca3a9ce
arnauddri <arnaud.drizard@gmail.com>
2015-01-19 09:17:32 +0100

    karatsuba wip)

--- maths/karatsuba/karatsuba.go
+++ maths/karatsuba/karatsuba.go
@@ -6,17 +6,31 @@ import (
 	"math/big"
 )
 
-func multiply(a, b *big.Int) *big.Int {
+func k_multiply(a, b *big.Int) *big.Int {
 	fmt.Println(a)
+	fmt.Println(a.BitLen())
+	m := _pivot(a, b)
+	splitA := _split(a, uint(m))
+	leftA, rightA := splitA[0], splitA[1]
+
+	splitB := _split(b, uint(m))
+	leftB, rightB := splitB[0], splitB[1]
+
+	fmt.Println(leftB, rightB, leftA, rightA)
 	return a
 }
 
 func _split(a *big.Int, m uint) []*big.Int {
 	big_a := big.NewInt(a.Int64())
-
 	left := big.NewInt(int64(a.Uint64())).Rsh(big_a, m)
 
-	return []*big.Int{left}
+	tempRight := big.NewInt(left.Int64())
+	tempRight = tempRight.Rsh(tempRight, m)
+
+	_right := big.NewInt(a.Int64())
+	right := _right.Sub(_right, tempRight)
+
+	return []*big.Int{left, right}
 }
 
 func _pivot(a, b *big.Int) int {
@@ -24,8 +38,16 @@ func _pivot(a, b *big.Int) int {
 	bitLen_b := b.BitLen()
 
 	if bitLen_a < bitLen_b {
-		return bitLen_a / 2
+		return bitLen_a >> 1
 	} else {
-		return bitLen_b / 2
+		return bitLen_b >> 1
 	}
+}
+
+func multiply(a, b *big.Int) *big.Int {
+	a1 := big.NewInt(a.Int64())
+	b1 := big.NewInt(b.Int64())
+
+	c := big.NewInt(0)
+	return c.Mul(a1, b1)
 }
--- maths/karatsuba/karatsuba_test.go
+++ maths/karatsuba/karatsuba_test.go
@@ -10,13 +10,6 @@ import (
 func TestMultipy(t *testing.T) {
 	a := big.NewInt(10)
 	b := big.NewInt(10)
-	fmt.Println(classic_multiply(a, b))
-}
-
-func classic_multiply(a, b *big.Int) *big.Int {
-	a1 := big.NewInt(a.Int64())
-	b1 := big.NewInt(b.Int64())
-
-	c := big.NewInt(0)
-	return c.Mul(a1, b1)
+	//fmt.Println(multiply(a, b))
+	fmt.Println(k_multiply(a, b))
 }
