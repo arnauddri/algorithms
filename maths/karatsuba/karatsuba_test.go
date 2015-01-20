e1728d647ca30057766fd9805c525509ae76a72b
arnauddri <arnaud.drizard@gmail.com>
2015-01-20 09:12:51 +0100

    benchmark karatsuba

--- maths/karatsuba/karatsuba_test.go
+++ maths/karatsuba/karatsuba_test.go
@@ -9,10 +9,23 @@ import (
 
 func TestMultipy(t *testing.T) {
 	a := big.NewInt(1234)
-	b := big.NewInt(1234)
 
-	if k_multiply(a, b).Cmp(mul(a, b)) != 0 {
-		fmt.Println(k_multiply(a, b), mul(a, b))
+	if k_multiply(a, a).Cmp(mul(a, a)) != 0 {
+		fmt.Println(k_multiply(a, a), mul(a, a))
 		t.Fatal("Wrong result")
+	}
+}
+
+func BenchmarkMultiply(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		a := big.NewInt(1234567890123456789)
+		mul(a, a)
+	}
+}
+
+func BenchmarkKaratsubaMultiply(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		a := big.NewInt(1234567890123456789)
+		k_multiply(a, a)
 	}
 }
