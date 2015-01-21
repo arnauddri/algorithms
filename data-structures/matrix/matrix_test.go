13ec8472843aad6a1c2c9c12c7b7edeaa859dfc5
arnauddri <arnaud.drizard@gmail.com>
2015-01-21 19:51:59 +0100

    matrices test fixed

--- data-structures/matrix/matrix_test.go
+++ data-structures/matrix/matrix_test.go
@@ -6,33 +6,33 @@ import (
 
 func TestMakeMatrix(t *testing.T) {
 	a := []float64{1, 2, 3, 4, 5, 6}
-	A := makeMatrix(a, 3, 2)
+	A := MakeMatrix(a, 3, 2)
 
 	if A.cols != 2 ||
 		A.rows != 3 ||
-		!FloatArrayEquals(A.elements, a) {
+		!FloatArrayEquals(A.Elements, a) {
 		t.Error()
 	}
 }
 
 func TestCount(t *testing.T) {
 	a := []float64{1, 2, 3, 4, 5, 6}
-	A := makeMatrix(a, 3, 2)
+	A := MakeMatrix(a, 3, 2)
 
-	if A.countRows() != 3 {
+	if A.CountRows() != 3 {
 		t.Error()
 	}
-	if A.countCols() != 2 {
+	if A.CountCols() != 2 {
 		t.Error()
 	}
 }
 
 func TestGetElm(t *testing.T) {
 	a := []float64{1, 2, 3, 4, 5, 6}
-	A := makeMatrix(a, 3, 2)
+	A := MakeMatrix(a, 3, 2)
 
 	for i := 0; i < 3; i++ {
-		if A.getElm(0, i) != float64(i+1) {
+		if A.GetElm(0, i) != float64(i+1) {
 			t.Error()
 		}
 	}
@@ -40,18 +40,18 @@ func TestGetElm(t *testing.T) {
 
 func TestSetElm(t *testing.T) {
 	a := []float64{1, 2, 3, 4, 5, 6}
-	A := makeMatrix(a, 3, 2)
+	A := MakeMatrix(a, 3, 2)
 
-	A.setElm(0, 0, 10)
+	A.SetElm(0, 0, 10)
 
-	if A.getElm(0, 0) != 10 {
+	if A.GetElm(0, 0) != 10 {
 		t.Error()
 	}
 }
 
 func TestTrace(t *testing.T) {
 	a := []float64{1, 2, 3, 4}
-	A := makeMatrix(a, 2, 2)
+	A := MakeMatrix(a, 2, 2)
 
 	if A.trace() != 5 {
 		t.Error()
@@ -60,34 +60,34 @@ func TestTrace(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	a := []float64{1, 1, 1, 1}
-	A := makeMatrix(a, 2, 2)
-	B := makeMatrix([]float64{2, 2, 2, 2}, 2, 2)
+	A := MakeMatrix(a, 2, 2)
+	B := MakeMatrix([]float64{2, 2, 2, 2}, 2, 2)
 
 	A.add(A)
-	if !FloatArrayEquals(A.elements, B.elements) {
+	if !FloatArrayEquals(A.Elements, B.Elements) {
 		t.Error()
 	}
 }
 
 func TestSubstract(t *testing.T) {
 	a := []float64{1, 1, 1, 1}
-	A := makeMatrix(a, 2, 2)
-	B := makeMatrix([]float64{2, 2, 2, 2}, 2, 2)
+	A := MakeMatrix(a, 2, 2)
+	B := MakeMatrix([]float64{2, 2, 2, 2}, 2, 2)
 
 	B.substract(A)
-	if !FloatArrayEquals(A.elements, B.elements) {
+	if !FloatArrayEquals(A.Elements, B.Elements) {
 		t.Error()
 	}
 }
 
 func TestScale(t *testing.T) {
 	a := []float64{1, 1, 1, 1}
-	A := makeMatrix(a, 2, 2)
-	B := makeMatrix([]float64{2, 2, 2, 2}, 2, 2)
+	A := MakeMatrix(a, 2, 2)
+	B := MakeMatrix([]float64{2, 2, 2, 2}, 2, 2)
 
 	A.scale(2)
 
-	if !FloatArrayEquals(A.elements, B.elements) {
+	if !FloatArrayEquals(A.Elements, B.Elements) {
 		t.Error()
 	}
 }
