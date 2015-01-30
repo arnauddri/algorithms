41ad7f4cd927e8c7d7a8f5e5af6cef162db48499
arnauddri <arnaud.drizard@gmail.com>
2015-01-30 20:56:35 +0100

    Queue: add mutex back

--- data-structures/queue/queue.go
+++ data-structures/queue/queue.go
@@ -1,39 +1,56 @@
 package queue
 
+import "sync"
+
 type Queue struct {
 	queue []interface{}
 	len   int
+	lock  *sync.Mutex
 }
 
 func New() *Queue {
 	queue := &Queue{}
 	queue.queue = make([]interface{}, 0)
 	queue.len = 0
+	queue.lock = new(sync.Mutex)
 
 	return queue
 }
 
 func (q *Queue) Len() int {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
 	return q.len
 }
 
 func (q *Queue) isEmpty() bool {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
 	return q.len == 0
 }
 
 func (q *Queue) Shift() (el interface{}) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
 	el, q.queue = q.queue[0], q.queue[1:]
 	q.len--
 	return
 }
 
 func (q *Queue) Push(el interface{}) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
 	q.queue = append(q.queue, el)
 	q.len++
-
-	return
 }
 
 func (q *Queue) Peek() interface{} {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
 	return q.queue[0]
 }
