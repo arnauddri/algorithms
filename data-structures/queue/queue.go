01488aafbf9643b7109708bf5bcbbdc5c981bdba
arnauddri <arnaud.drizard@gmail.com>
2015-01-30 20:44:27 +0100

    Queue: mutex removed for now

--- data-structures/queue/queue.go
+++ data-structures/queue/queue.go
@@ -1,11 +1,8 @@
 package queue
 
-import "sync"
-
 type Queue struct {
 	queue []interface{}
 	len   int
-	lock  *sync.Mutex
 }
 
 func New() *Queue {
@@ -17,39 +14,26 @@ func New() *Queue {
 }
 
 func (q *Queue) Len() int {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-
 	return q.len
 }
 
 func (q *Queue) isEmpty() bool {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-
 	return q.len == 0
 }
 
 func (q *Queue) Shift() (el interface{}) {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-
 	el, q.queue = q.queue[0], q.queue[1:]
 	q.len--
 	return
 }
 
 func (q *Queue) Push(el interface{}) {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-
 	q.queue = append(q.queue, el)
 	q.len++
+
+	return
 }
 
 func (q *Queue) Peek() interface{} {
-	q.lock.Lock()
-	defer q.lock.Unlock()
-
 	return q.queue[0]
 }
