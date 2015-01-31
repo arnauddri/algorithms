aef5d65cc3dac79b5de2de4c2555c6ddc3effcd0
arnauddri <arnaud.drizard@gmail.com>
2015-01-31 14:08:57 +0100

    hash-tables: refactoring

--- data-structures/hash-tables/ht.go
+++ data-structures/hash-tables/ht.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/arnauddri/algorithms/data-structures/linked-list"
+	"math"
 )
 
 type HashTable struct {
@@ -72,4 +73,14 @@ func (ht *HashTable) find(i int, key string) (*item, error) {
 	}
 
 	return value.Value.(*item), nil
+}
+
+// Horner's Method to hash string of length L (O(L))
+func hashCode(s string) int {
+	hash := int32(0)
+	for i := 0; i < len(s); i++ {
+		hash = int32(hash<<5-hash) + int32(s[i])
+		hash &= hash
+	}
+	return int(math.Abs(float64(hash)))
 }
--- data-structures/hash-tables/ht_test.go
+++ data-structures/hash-tables/ht_test.go
@@ -13,3 +13,12 @@ func TestHt(t *testing.T) {
 
 	fmt.Println(val)
 }
+
+func TestHash(t *testing.T) {
+	test := "Hello World!"
+
+	if hashCode(test) != 969099747 {
+		fmt.Println(hashCode(test))
+		t.Error()
+	}
+}
